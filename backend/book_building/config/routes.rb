9c55ce07982cc9e671ac01c7cece1eb5f4bb7ec0
longdang0497 <danghoanglong3004@gmail.com>
2020-04-14 09:32:57 +0700

    Update css

--- backend/book_building/config/routes.rb
+++ backend/book_building/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "home#index"
-  match "*path" => redirect("/"), via: :all
+  match "/admins/sign_up" => redirect("/"), via: :all
 
   resources :buildings
   resources :rooms do
